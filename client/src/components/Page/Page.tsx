8015e5ccaccfe3254390bfed54ff3ee669fead03
based4real <45218699+based4real@users.noreply.github.com>
2024-11-27 22:14:08 +0100

    Tilføjet en default page + omskrevet login til ta bruge Radix

--- client/src/components/Page/Page.tsx
+++ client/src/components/Page/Page.tsx
@@ -1,16 +1,20 @@
-import { Box, Container } from "@radix-ui/themes";
+import { Flex } from "@radix-ui/themes";
 import { ReactNode } from "react";
+import Background from "./Background";
 
 type PageProps = {
   children: ReactNode;
+  background?: boolean;
+  align?: "start" | "center" | "end";
+  justify?: "start" | "center" | "end";
+  className?: string;
 };
 
-export default function Page({ children }: PageProps) {
+export default function Page({ children, background = true, align = "center", justify = "center", className = ""}: PageProps) {
   return (
-    <Box>
-      <Container>
-        {children}
-      </Container>
-    </Box>
+    <Flex className={className} align={align} justify={justify} height="100vh" width="100vw">
+      {background && <Background />}
+      {children}
+    </Flex>
   );
 }

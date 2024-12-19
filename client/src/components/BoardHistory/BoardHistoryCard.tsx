b35520c8bca52bdf03d04b8e8ecf73af1490620c
based4real <45218699+based4real@users.noreply.github.com>
2024-12-19 17:47:31 +0100

    fikset visning af telefon

--- client/src/components/BoardHistory/BoardHistoryCard.tsx
+++ client/src/components/BoardHistory/BoardHistoryCard.tsx
@@ -1,10 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import { Card, Flex, Text, Badge, Link } from '@radix-ui/themes';
+import React, {Fragment, useEffect, useState} from 'react';
+import { Card, Flex, Text, Badge, Link, Separator } from '@radix-ui/themes';
 import { format } from 'date-fns';
 import { BalanceAction, BalanceHistoryEntryResponse, getUserDetailsAtom, UserDetailsResponse, AppRoutes, BoardHistoryResponse } from '../import';
-import {useAtom} from "jotai/index";
 import { da } from 'date-fns/locale';
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
 
 interface BoardHistoryCardProps {
     entry: BoardHistoryResponse;
@@ -12,52 +13,38 @@ interface BoardHistoryCardProps {
 }
 
 export default function BoardHistoryCard({ entry, showUserEmail }: BoardHistoryCardProps) {
-    const [getUserDetails] = useAtom(getUserDetailsAtom);
-    const [userDetails, setUserDetails] = useState<UserDetailsResponse | null>(null);
-    const navigate = useNavigate();
-    
-    /*
-    useEffect(() => {
-        const loadUserDetails = async () => {
-            const user = await getUserDetails(entry.userId);
-            if (user) {
-                setUserDetails(user);
-            }
-        };
-
-        loadUserDetails();
-    }, [entry.userId, getUserDetails]);*/
-    
-    const actionColors = {
-        [BalanceAction.UserBought]: 'green',
-        [BalanceAction.UserUsed]: 'red',
-        [BalanceAction.AdminAssigned]: 'blue',
-        [BalanceAction.AdminRevoked]: 'orange',
-        [BalanceAction.WonPrize]: 'purple',
-    } as const;
-
-    const actionText = {
-        [BalanceAction.UserBought]: 'Bruger købte',
-        [BalanceAction.UserUsed]: 'Bruger brugte',
-        [BalanceAction.AdminAssigned]: 'Admin tildelte',
-        [BalanceAction.AdminRevoked]: 'Admin fratog',
-        [BalanceAction.WonPrize]: 'Vandt præmie',
-    };
-
     return (
         <Card className="p-4">
             <Flex direction="column" gap="2">
-                <Flex justify="between" align="center">
-                    <Badge variant="soft">
-                        
+            <Flex direction="row" align="center" justify="between" wrap="nowrap" gap="4" className="w-full">
+                <Flex direction="row" align="center" wrap="wrap">
+                    {!entry.isActiveGame &&
+                    <>
+                    <Badge className='p-1 mr-2' color={`${entry.wasWin ? 'green' : 'red'}`}>
+                        <FontAwesomeIcon size="lg" icon={entry.wasWin ? faCheckCircle : faXmarkCircle} />
                     </Badge>
-                    <Text size="5" weight="bold">
-                        {entry.configuration}
-                    </Text>
+                    <Separator className='mr-2' orientation="vertical"/>
+                    </>
+                }
+                    {entry.configuration?.map((num, i) => (
+                        <Fragment key={i}>
+                            <Badge 
+                                className="flex-none" 
+                                color={`${entry.isActiveGame ? 'gray' : entry.wasWin ? 'green' : 'red'}`} 
+                                size="1"
+                            >
+                                {num}
+                            </Badge>
+                            {i < (entry.configuration?.length || 0) - 1 && <Text>-</Text>}
+                        </Fragment>
+                    ))}
                 </Flex>
-
+                <Text size="5" weight="bold" className="ml-auto">
+                    {entry.price},-
+                </Text>
+            </Flex>
                 <Text size="2" color="gray">
-                    {format(new Date(entry.placedOn), 'd. MMMM yyyy HH:mm', { locale: da })}
+                    Uge: {entry.gameWeek} - {format(new Date(entry.placedOn), 'd. MMMM yyyy HH:mm', { locale: da })}
                 </Text>
             </Flex>
         </Card>

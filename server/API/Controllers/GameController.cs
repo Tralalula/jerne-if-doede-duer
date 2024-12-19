546703f680901f9b5ed950bfd554be31343ebd12
based4real <45218699+based4real@users.noreply.github.com>
2024-12-19 18:12:26 +0100

    opsat ny service samt controller

---
+++ server/API/Controllers/GameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GameController(IGameService service) : ControllerBase
+{
+    
+}
---
+++ server/Service/GameService.cs
@@ -0,0 +1,11 @@
+﻿namespace Service;
+
+public interface IGameService
+{
+    
+}
+
+public class GameService : IGameService
+{
+    
+}

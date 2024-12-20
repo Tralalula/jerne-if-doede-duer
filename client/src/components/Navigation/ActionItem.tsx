514f6f989e6e83f3b46ee67e44c072448ddf6038
Tobias <qenzoz@gmail.com>
2024-12-20 10:09:48 +0100

    Fiks hover bredde ActionItem

--- client/src/components/Navigation/ActionItem.tsx
+++ client/src/components/Navigation/ActionItem.tsx
@@ -14,25 +14,30 @@ interface ActionItemProps {
 
 export const ActionItem = forwardRef<HTMLButtonElement, ActionItemProps>(
     ({ className, children, title, icon, onClick, ...props }, ref) => (
-        <li>
+        <li className="w-full">
             <NavigationMenu.Link asChild>
                 <button
-                    className={classNames("ListItemLink", className)}
+                    className={classNames(
+                        "w-full block select-none rounded-md no-underline outline-none transition-colors",
+                        className
+                    )}
                     onClick={onClick}
                     ref={ref}
                     type="button"
                     {...props}
                 >
-                    <div className="flex items-start">
-                        {icon && (
-                            <FontAwesomeIcon
-                                icon={icon}
-                                className="mr-2 text-red11"
-                            />
-                        )}
-                        <div>
-                            <div className="ListItemHeading">{title}</div>
-                            <p className="ListItemText">{children}</p>
+                    <div className="w-full p-3 hover:bg-mauve3 rounded-md">
+                        <div className="flex items-start">
+                            {icon && (
+                                <FontAwesomeIcon
+                                    icon={icon}
+                                    className="mr-2 text-red11"
+                                />
+                            )}
+                            <div>
+                                <div className="ListItemHeading text-[15px] leading-none">{title}</div>
+                                <p className="ListItemText">{children}</p>
+                            </div>
                         </div>
                     </div>
                 </button>
@@ -40,5 +45,6 @@ export const ActionItem = forwardRef<HTMLButtonElement, ActionItemProps>(
         </li>
     )
 );
+
 
 ActionItem.displayName = "ActionItem";

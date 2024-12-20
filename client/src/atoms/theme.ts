ee0dd2b457c07cd178903854e07b7136dc58fe00
Tobias <qenzoz@gmail.com>
2024-12-20 09:15:06 +0100

    Juster så man starter i darkmode

--- client/src/atoms/theme.ts
+++ client/src/atoms/theme.ts
@@ -3,6 +3,6 @@ import { atom } from 'jotai';
 type RadixTheme = "light" | "dark";
 
 const initialTheme: RadixTheme =
-  (localStorage.getItem('theme') as RadixTheme) || "light";
+  (localStorage.getItem('theme') as RadixTheme) || "dark";
 
 export const themeAtom = atom<RadixTheme>(initialTheme);
